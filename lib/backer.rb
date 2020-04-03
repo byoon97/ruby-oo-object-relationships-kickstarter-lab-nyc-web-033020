0a7da4e74359951caf0841d03f4c9c465ea67144
Brandon Yoon <byoon5397@gmail.com>
2020-04-03 18:52:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -19,7 +19,7 @@ class Backer
     project_arr = []
     ProjectBacker.all.select do |project|
       if project.backer == self
-        project_arr << project.title
+        project_arr << Project.titles
       end
     end
   end
